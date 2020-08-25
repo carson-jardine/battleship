bc0e47231a8cb5662203a65632a339ecc55d6b3b
Carson Jardine <carsonjardine@gmail.com>
2020-08-25 16:47:40 -0600

    Add test to see it exists

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -3,6 +3,32 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 
-class CellTest < Minitest::Test 
+class CellTest < Minitest::Test
+  def setup
+    @cell = Cell.new("B4")
+  end
 
+  def test_it_exists
+    assert_instance_of Cell, @cell 
+  end
+
+  # cell.coordinate
+  # # => "B4"
+  #
+  # cell.ship
+  # # => nil
+  #
+  # cell.empty?
+  # # => true
+  #
+  # cruiser = Ship.new("Cruiser", 3)
+  # # => #<Ship:0x00007f84f0891238...>
+  #
+  # cell.place_ship(cruiser)
+  #
+  # cell.ship
+  # # => #<Ship:0x00007f84f0891238...>
+  #
+  # cell.empty?
+  # # => false
 end
