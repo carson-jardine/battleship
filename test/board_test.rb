88ea172b317177262a60191a811b185fcdb740d8
Kate Tester <katemorris@gmail.com>
2020-08-26 15:49:51 -0600

    Adds test to validate length of ship placement.

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,6 +8,8 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
@@ -25,6 +27,12 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A5")
     assert_equal false, @board.valid_coordinate?("E1")
     assert_equal false, @board.valid_coordinate?("A22")
+  end
+
+  def test_valid_length_placement
+    assert_equal false, @board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(submarine, ["A2", "A3", "A4"])
+
   end
 
 end
