25019f51c3ad8a4b2ba0e2e5afee76e58aed1af4
Kate Tester <katemorris@gmail.com>
2020-08-27 15:51:05 -0600

    Adds tests and method for shot results messages.

--- lib/message.rb
+++ lib/message.rb
@@ -8,4 +8,44 @@ class Message
     p "Welcome to BATTLESHIP \nEnter p to play. Enter q to quit."
   end
 
+  def hooman_instructions
+    p "I have laid out my ships on the grid. \n
+    You now need to lay out your two ships. \n
+    The Cruiser is three units long and the Submarine is two units long."
+  end
+
+  def hooman_cruiser_instructions
+    p "Enter the squares for the Cruiser (3 spaces): \n> "
+  end
+
+  def hooman_sub_instructions
+    p "Enter the squares for the Submarine (2 spaces): \n> "
+  end
+
+  def invalid_coordinate_entry
+    p "Those are invalid coordinates. Please try again: \n> "
+  end
+
+  def hooman_shot_coordinate_entry
+    p "Enter the coordinate for your shot: \n> "
+  end
+
+  def hooman_valid_shot_entry
+    p "Please enter a valid coordinate: \n> "
+  end
+
+  def hooman_shot_results(cell)
+    shot_type = nil
+    if cell.render == "M"
+      shot_type = "miss"
+    elsif cell.render == "X"
+      shot_type = "hit, the ship is sunk"
+    elsif cell.render == "H"
+      shot_type = "hit"
+    else
+      shot_type = "WTF PPL"
+    end
+    p "Your shot on #{cell.coordinate} was a #{shot_type}."
+  end
+
 end
--- test/message_test.rb
+++ test/message_test.rb
@@ -30,4 +30,55 @@ class MessageTest < Minitest::Test
     assert_equal expected, @message.hooman_instructions
   end
 
+  def test_hooman_cruiser_instructions
+    expected = "Enter the squares for the Cruiser (3 spaces): \n> "
+
+    assert_equal expected, @message.hooman_cruiser_instructions
+  end
+
+  def test_hooman_sub_instructions
+    expected = "Enter the squares for the Submarine (2 spaces): \n> "
+
+    assert_equal expected, @message.hooman_sub_instructions
+  end
+
+  def test_invalid_coordinate_user_input
+    expected = "Those are invalid coordinates. Please try again: \n> "
+
+    assert_equal expected, @message.invalid_coordinate_entry
+  end
+
+  def test_hooman_instructions_to_enter_shot_coordinates
+    expected = "Enter the coordinate for your shot: \n> "
+
+    assert_equal expected, @message.hooman_shot_coordinate_entry
+  end
+
+  def test_hooman_instructions_input_valid_shot_coordinates
+    expected = "Please enter a valid coordinate: \n> "
+
+    assert_equal expected, @message.hooman_valid_shot_entry
+  end
+
+  def test_message_to_hooman_shot
+    cruiser = Ship.new("Cruiser", 3)
+    cell_1 = Cell.new("A4")
+    cell_1.fire_upon
+
+    expected = "Your shot on A4 was a miss."
+    assert_equal expected, @message.hooman_shot_results(cell_1)
+
+    cell_1.place_ship(cruiser)
+    cell_1.fire_upon
+    expected = "Your shot on A4 was a hit."
+
+    assert_equal expected, @message.hooman_shot_results(cell_1)
+
+    cruiser.hit
+    cruiser.hit
+    expected = "Your shot on A4 was a hit, the ship is sunk."
+
+    assert_equal expected, @message.hooman_shot_results(cell_1)
+  end
+
 end
