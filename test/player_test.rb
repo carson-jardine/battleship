b177a61465df304035e4115df0c6c3ad52d72205
Kate Tester <katemorris@gmail.com>
2020-08-27 16:54:49 -0600

    Adds player class and test files

---
+++ test/player_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/ship'
+require './lib/cell'
+require './lib/message'
+require './lib/game'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+
+  def setup
+
+  end
+
+  def test_it_exists
+
+  end
+
+end
