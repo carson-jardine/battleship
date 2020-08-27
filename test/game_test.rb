60d62a1da3beaac2d720166adf1a5fcbfd8c45bb
Kate Tester <katemorris@gmail.com>
2020-08-27 13:55:51 -0600

    WIP on cpu can place ships test

--- test/game_test.rb
+++ test/game_test.rb
@@ -9,10 +9,19 @@ class GameTest < Minitest::Test
 
   def setup
     @game = Game.new
+    @board = Board.new
+    @cpu_cruiser = Ship.new("Cruiser", 3)
+    @hooman_cruiser = Ship.new("Cruiser", 3)
+    @cpu_sub = Ship.new("Submarine", 2)
+    @hooman_sub = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
     assert_instance_of Game, @game
+  end
+
+  def test_cpu_can_place_ships
+    @game.cpu_place_ship(cpu_cruiser)
   end
 
 end
