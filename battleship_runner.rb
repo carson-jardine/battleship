e79703bdd1832f111a0cdda88b4d4400f1a43759
Kate Tester <katemorris@gmail.com>
2020-08-27 13:41:47 -0600

    Adds logic to start the game.

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,4 +1,13 @@
-require './lib/board'
-require './lib/ship'
-require './lib/cell'
 require './lib/game'
+
+puts "Welcome to BATTLESHIP"
+puts "Enter p to play. Enter q to quit."
+play = $stdin.gets.chomp.downcase
+
+if play == "p"
+  start
+elsif play == "q"
+  exit
+else
+  puts "Do what?"
+end
