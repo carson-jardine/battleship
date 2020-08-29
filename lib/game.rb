3f92195826b3eb9f9479c25c8a1829958835ae3c
Carson Jardine <carsonjardine@gmail.com>
2020-08-28 18:54:36 -0600

    Adds run game method and display boards method

--- lib/game.rb
+++ lib/game.rb
@@ -16,15 +16,27 @@ class Game
   def start
     @message.main_menu
     user_input = gets.chomp.downcase
-
     if user_input == "p"
-      @cpu.cpu_place_ships
-      #hooman.place_ships
+      run_game
     elsif user_input == "q"
       exit
     else
-      puts "Do you listen???"
+      puts "Do you listen??? Let's try again"
+      start
     end
+  end
+
+  def run_game
+    @cpu.cpu_place_ships
+    @hooman.hooman_place_ships
+    display_boards
+  end
+
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    print @cpu.board.render
+    puts "==============HOOMAN BOARD=============="
+    print @hooman.board.render(true)
   end
 
 end
--- lib/player.rb
+++ lib/player.rb
@@ -3,27 +3,28 @@ require './lib/board'
 require './lib/message'
 
 class Player
-  attr_reader :board, :cruiser, :submarine
+  attr_reader :board, :cruiser, :submarine, :message
 
   def initialize
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
+    @message = Message.new
   end
 
   def cpu_place_cruiser
-    @cpu_cruiser = @board.cells.keys.shuffle[0..2]
-    if @board.valid_placement?(@cruiser, @cpu_cruiser)
-      @board.place(@cruiser, @cpu_cruiser)
+    cpu_cruiser = @board.cells.keys.shuffle[0..2]
+    if @board.valid_placement?(@cruiser, cpu_cruiser)
+      @board.place(@cruiser, cpu_cruiser)
     else
       cpu_place_cruiser
     end
   end
 
   def cpu_place_sub
-    @cpu_sub = @board.cells.keys.shuffle[0..1]
-    if @board.valid_placement?(@submarine, @cpu_sub)
-      @board.place(@submarine, @cpu_sub)
+    cpu_sub = @board.cells.keys.shuffle[0..1]
+    if @board.valid_placement?(@submarine, cpu_sub)
+      @board.place(@submarine, cpu_sub)
     else
       cpu_place_sub
     end
@@ -33,5 +34,39 @@ class Player
     cpu_place_cruiser
     cpu_place_sub
   end
+
+  def hooman_place_ships
+    @message.hooman_instructions
+    puts @board.render
+    hooman_place_cruiser
+    hooman_place_sub
+  end
+
+  def hooman_place_cruiser
+    @message.hooman_cruiser_instructions
+
+    cruiser_input = gets.chomp.upcase.split(" ")
+    if @board.valid_placement?(@cruiser, cruiser_input)
+      @board.place(@cruiser, cruiser_input)
+      puts @board.render(true)
+    else
+      @message.invalid_coordinate_entry
+      hooman_place_cruiser
+    end
+  end
+
+  def hooman_place_sub
+    @message.hooman_sub_instructions
+
+    sub_input = gets.chomp.upcase.split(" ")
+    if @board.valid_placement?(@submarine, sub_input)
+      @board.place(@submarine, sub_input)
+      puts @board.render(true)
+    else
+      @message.invalid_coordinate_entry
+      hooman_place_sub
+    end
+  end
+
 
 end
